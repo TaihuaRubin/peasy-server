0aeed376d7abdaa3ec3d426c04abd18fb1ec9dd2
Taihua Rubin <TaihuaRubin@gmail.com>
2020-08-31 12:49:19 -0400

    put in console.log inside auth logiin

--- server/auth/index.js
+++ server/auth/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { User } = require("../db/models");
 
 router.post("/login", async (req, res, next) => {
+  console.log("hitting logging");
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ where: { email } });

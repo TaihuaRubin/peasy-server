5c0a1a8482451cf9ff7ce207ec64bf4424b40c34
Taihua Rubin <TaihuaRubin@gmail.com>
2020-08-31 17:17:42 -0400

    added list routes

--- server/api/lists.js
+++ server/api/lists.js
@@ -1,6 +1,5 @@
-const router = require('express').Router()
-const { ListAccess, ItemUserList, Item } = require('../db/models')
-
+const router = require("express").Router();
+const { ListAccess, ItemUserList, Item } = require("../db/models");
 
 //all the lists
 // router.get('/', async (req, res, next) => {
@@ -12,55 +11,83 @@ const { ListAccess, ItemUserList, Item } = require('../db/models')
 //     }
 // })
 
-
 // route for api/lists/:listId
-router.get('/private/:userId', async (req, res, next) => {
-    try {
-        const listPrivate = await ListAccess.findOne({where: {
-            userId: req.params.userId,
-            category: 'private'
-        },
-    })
-        const listItems = await ItemUserList.findAll({
-            where: {listId: listPrivate.listId},
-            include: {model: Item}
-        })
-        res.json(listItems)
-    } catch (error) {
-        next(error)
-    }
-})
+router.get("/private/:userId", async (req, res, next) => {
+  try {
+    const listPrivate = await ListAccess.findOne({
+      where: {
+        userId: req.params.userId,
+        category: "private",
+      },
+    });
+    const listItems = await ItemUserList.findAll({
+      where: { listId: listPrivate.listId },
+      include: { model: Item },
+    });
+    res.json(listItems);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/household/:listId', async (req, res, next) => {
-    try {
-        const listItems = await ItemUserList.findAll({
-            where: {listId: req.params.listId},
-            include: {model: Item}
-        })
-        res.json(listItems)
-    } catch (error) {
-        next(error)
-    }
-})
+router.get("/household/:listId", async (req, res, next) => {
+  try {
+    const listItems = await ItemUserList.findAll({
+      where: { listId: req.params.listId },
+      include: { model: Item },
+    });
+    res.json(listItems);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newList = await List.create(req.body);
+    res.json(newList);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:listId", async (req, res, next) => {
+  try {
+    const list = await List.findOne({
+      where: {
+        id: req.params.listId,
+      },
+      include: {
+        model: ListAccess,
+        where: {
+          category: "Household",
+        },
+      },
+    });
+    res.json(list);
+  } catch (error) {
+    next(error);
+  }
+});
 
 //update item quantity
 router.put("/:listId/:itemId", async (req, res, next) => {
-    try{
-        console.log("in put request")
-        console.log("this is req.body", req.body)
-        const item = await ItemUserList.findOne({
-            where: {
-                listId: req.params.listId,
-                itemId: req.params.itemId
-            }
-        })
-        console.log("this is the item we found to update",item)
-        const updatedItem = await item.update({quantity: req.body.quantity})
-        console.log("this is updateditem", updatedItem)
-        res.json(updatedItem)
-    }catch(error){
-        console.log(error)
-    }
-})
+  try {
+    console.log("in put request");
+    console.log("this is req.body", req.body);
+    const item = await ItemUserList.findOne({
+      where: {
+        listId: req.params.listId,
+        itemId: req.params.itemId,
+      },
+    });
+    console.log("this is the item we found to update", item);
+    const updatedItem = await item.update({ quantity: req.body.quantity });
+    console.log("this is updateditem", updatedItem);
+    res.json(updatedItem);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router
+module.exports = router;

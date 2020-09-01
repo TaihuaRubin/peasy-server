3e2cf0e0a5054dcb4f624f13c5542d80679ba6fc
Taihua Rubin <TaihuaRubin@gmail.com>
2020-09-01 12:41:47 -0400

    updated create hosuehold server side

--- server/api/households.js
+++ server/api/households.js
@@ -2,23 +2,23 @@ const router = require("express").Router();
 const { User, ListAccess, List, ItemUserList, Item, StorePreference, Store } = require("../db/models");
 
 //all the lists
-router.get('/', async (req, res, next) => {
-    try {
-        const lists = await List.findAll()
-        res.json(lists)
-    } catch (error) {
-        next(error)
-    }
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const lists = await List.findAll();
+    res.json(lists);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/:userId", async (req, res, next) => {
   try {
     const households = await ListAccess.findAll({
       where: {
         userId: req.params.userId,
-        category: "household"
-       },
-       include: List
+        category: "household",
+      },
+      include: List,
     });
     res.json(households);
   } catch (error) {
@@ -26,4 +26,13 @@ router.get("/:userId", async (req, res, next) => {
   }
 });
 
-module.exports = router
+router.post("/:userId", async (req, res, next) => {
+  try {
+    const newHousehold = await ListAccess.create(req.body);
+    res.json(newHousehold);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;

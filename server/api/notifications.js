6a36a2f3d0bdc6db9e750b71cb989b0103a998cb
Taihua Rubin <TaihuaRubin@gmail.com>
2020-09-02 11:48:21 -0400

    added notification read route

--- server/api/notifications.js
+++ server/api/notifications.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const { Notification} = require("../db/models");
+const { Notification } = require("../db/models");
 
 router.get("/:userId", async (req, res, next) => {
   try {
     const notifications = await Notification.findAll({
       where: {
         userId: req.params.userId,
-       }
+      },
     });
     res.json(notifications);
   } catch (error) {
@@ -14,4 +14,20 @@ router.get("/:userId", async (req, res, next) => {
   }
 });
 
-module.exports = router
+router.put("/read", async (req, res, next) => {
+  try {
+    const notificationId = req.body.notificationId;
+    const toUpdate = await Notification.findById(notificationId);
+    const userId = toUpdate.userId;
+    toUpdate.unread = false;
+    await toUpdate.save();
+
+    // send back all the noties
+    const updatedNotifications = await Notifications.findAll({ where: { userId: userId } });
+    res.json(updatedNotifications);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;

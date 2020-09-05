4583995e679209b63f2ef22fe2cc1485f59b380f
Taihua Rubin <TaihuaRubin@gmail.com>
2020-09-05 15:34:16 -0400

    updated itemUSerList table

--- server/db/models/itemUserList.js
+++ server/db/models/itemUserList.js
@@ -1,35 +1,30 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require("sequelize");
+const db = require("../db");
 
 const ItemUserList = db.define("itemUserList", {
-    // userId: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     validate: {
-    //         notEmpty: true
-    //     }
-    // },
-    // listId: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     validate: {
-    //         notEmpty: true
-    //     }
-    // },
-    // itemId: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     validate: {
-    //         notEmpty: true
-    //     }
-    // },
-    quantity: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        validate: {
-            notEmpty: true
-        }
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
+  },
+  purchased: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  paid: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
 });
 
 module.exports = ItemUserList;

33bcba2c8e8cd8f3e4fd1e1fecb7b990edb422fa
Taihua Rubin <TaihuaRubin@gmail.com>
2020-09-07 09:59:36 -0400

    fixing add route

--- server/api/items.js
+++ server/api/items.js
@@ -11,28 +11,8 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-//route for each single item:  "api/items/:id"
-router.get("/:itemId", async (req, res, next) => {
-  try {
-    const singleItem = await Item.findById(req.params.itemId);
-    res.json(singleItem);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//add new item to Item table
-router.post("/", async (req, res, next) => {
-  try {
-    const item = await Item.create(req.body);
-    res.json(item);
-  } catch (error) {
-    next(error);
-  }
-});
-
 //update item quantity
-router.put("/add", async (req, res, next) => {
+router.get("/add", async (req, res, next) => {
   try {
     const { itemId, listId, userId } = req.body;
     const item = await ItemUserList.findAll({
@@ -47,6 +27,26 @@ router.put("/add", async (req, res, next) => {
     res.send(item);
   } catch (error) {
     console.log(error);
+  }
+});
+
+//add new item to Item table
+router.post("/", async (req, res, next) => {
+  try {
+    const item = await Item.create(req.body);
+    res.json(item);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//route for each single item:  "api/items/:id"
+router.get("/:itemId", async (req, res, next) => {
+  try {
+    const singleItem = await Item.findById(req.params.itemId);
+    res.json(singleItem);
+  } catch (error) {
+    next(error);
   }
 });
 
